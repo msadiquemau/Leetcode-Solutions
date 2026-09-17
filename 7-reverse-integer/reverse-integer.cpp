46deeeae9f37fd117a89b3132872396cccf45038
Mohammad Sadique <sadiquemohammad897@gmail.com>
2026-09-17 23:25:00 +0530

    Time: 2 ms (38.22%) | Memory: 8.5 MB (54.29%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -8,9 +8,10 @@ public:
             ans = ans * 10 + digit;
             x = x / 10;
         }
-
+        
         if (ans > INT_MAX || ans < INT_MIN) {
             return 0;
+
         }
 
         return ans;
