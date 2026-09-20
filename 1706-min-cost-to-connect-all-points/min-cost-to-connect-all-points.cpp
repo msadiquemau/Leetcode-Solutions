68800bf5faae0820fdb2d383f440f429026482a6
Mohammad Sadique <sadiquemohammad897@gmail.com>
2026-09-21 00:11:56 +0530

    Time: 26 ms (87.37%) | Memory: 14.5 MB (95.34%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n = points.size();
+
+        vector<int> minDist(n, INT_MAX);
+        vector<bool> visited(n, false);
+
+        minDist[0] = 0;
+
+        int totalCost = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Minimum cost wala unvisited point choose karo
+            int u = -1;
+
+            for (int j = 0; j < n; j++) {
+                if (!visited[j] && (u == -1 || minDist[j] < minDist[u])) {
+                    u = j;
+                }
+            }
+
+            // MST me add
+            visited[u] = true;
+            totalCost += minDist[u];
+
+            // Baaki points ki minimum distance update karo
+            for (int v = 0; v < n; v++) {
+
+                if (!visited[v]) {
+
+                    int cost = abs(points[u][0] - points[v][0]) +
+                               abs(points[u][1] - points[v][1]);
+
+                    minDist[v] = min(minDist[v], cost);
+                }
+            }
+        }
+
+        return totalCost;
+    }
+};
